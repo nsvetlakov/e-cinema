9a5e115ace618053e5d4ae4c30624e773da68479
Nikita <mensaje42@gmail.com>
2020-09-06 14:49:31 +0400

    v 0.0.1

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+
+    //Замена текста в инпут с файлом
+    $('.input-file').each(function () {
+        var $input = $(this),
+            $label = $input.next('.fileName'),
+            labelVal = $label.html();
+
+        $input.on('change', function (element) {
+            var fileName = '';
+            if (element.target.value) fileName = element.target.value.split('\\').pop();
+            fileName ? $label.addClass('has-file').html(fileName) : $label.removeClass('has-file').html(labelVal);
+        });
+    });
+
+    $('.input-file-download').each(function () {
+        var $input = $(this),
+            $label = $input.next('.fileName'),
+            labelVal = $label.html();
+
+        $input.on('change', function (element) {
+            var fileName = '';
+            if (element.target.value) fileName = element.target.value.split('\\').pop();
+            fileName ? $label.addClass('has-file').html(fileName) : $label.removeClass('has-file').html(labelVal);
+        });
+    });
+
+    //=================================================================================================
+
+    // SelectBox
+    var selectBox = $('.icon[data-select]');
+    selectBox.each(function(){
+        icon = $(this);
+
+        icon.on('click', function(){
+            parent = $(this).parents('.selectBox');
+            trueSelect = parent.find('.trueSelect option');
+            selected =  parent.find('.selected');
+            
+            show = parent.find('.show');
+            showSpan = parent.find('.show span');
+            showHeight = showSpan.innerHeight() * showSpan.length + 40;
+            
+            parent.toggleClass('active');
+            show.toggleClass('active');
+            if(show.hasClass('active')){
+                show.css({'height': showHeight});
+            } else{
+                show.css({'height': 0});
+            }
+
+            show.on('click', 'span', function(){
+                text = $(this).text();
+                selected.text(text);
+                trueSelect.val(text).text(text);
+                parent.find('.icon[data-select]').trigger('click');
+            })            
+        })
+    })
+
+    //=================================================================================================
+
+
+
+});
+
+
